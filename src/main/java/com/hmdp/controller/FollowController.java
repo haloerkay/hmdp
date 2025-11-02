1125f25cd143991ef974f61d7c3007d5d7b40ae4
haloerkay <1422369232@qq.com>
2025-11-02 16:54:23 +0800

    git feat: 好友关注功能实现

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/follow")
 public class FollowController {
 
+    @Resource
+    private IFollowService followService;
+
+    // 关注/取关
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long id,@PathVariable("isFollow") Boolean isFollow){
+        return followService.follow(id,isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    // 判断是否关注
+    public Result isFollow(@PathVariable("id") Long id){
+        return followService.isFollow(id);
+    }
+
+    // 共同关注，找出当前用户与笔记作者之间共同关注
+    @GetMapping("/common/{id}")
+    public Result followCommons(@PathVariable("id") Long id){
+        return followService.followCommons(id);
+    }
 }
